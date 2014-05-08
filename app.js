3dadab54bd461facd8f0f5180cd01e299b66131d
Sierra Kaplan-Nelson <sierrakn@stanford.edu>
2014-05-08 16:58:18 -0700

    Fixed merge conflicts for real

--- app.js
+++ app.js
@@ -135,34 +135,34 @@ var server = http.createServer(app).listen(process.env.PORT || 3000, function(){
 
 var io = socket.listen(server);
 
-    // SOCKET IO
-    var active_connections = 0;
-    io.sockets.on('connection', function (socket) {
+// SOCKET IO
+var active_connections = 0;
+io.sockets.on('connection', function (socket) {
 
-        active_connections++;
-        console.log(active_connections);
+    active_connections++;
+    console.log(active_connections);
 
-        io.sockets.emit('user:connect', active_connections);
+    io.sockets.emit('user:connect', active_connections);
 
-        socket.on('disconnect', function () {
-            active_connections--;
-            io.sockets.emit('user:disconnect', active_connections);
-        });
-
-          // EVENT: User starts drawing something
-        socket.on('draw:progress', function (uid, co_ordinates) {
-            
-           io.sockets.emit('draw:progress', uid, co_ordinates)
-
-        });
-
-        // EVENT: User stops drawing something
-        socket.on('draw:end', function (uid, co_ordinates) {
-            
-          io.sockets.emit('draw:end', uid, co_ordinates)
-
-        });
-      
+    socket.on('disconnect', function () {
+        active_connections--;
+        io.sockets.emit('user:disconnect', active_connections);
     });
+
+      // EVENT: User starts drawing something
+    socket.on('draw:progress', function (uid, co_ordinates) {
+        
+       io.sockets.emit('draw:progress', uid, co_ordinates)
+
+    });
+
+    // EVENT: User stops drawing something
+    socket.on('draw:end', function (uid, co_ordinates) {
+        
+      io.sockets.emit('draw:end', uid, co_ordinates)
+
+    });
+  
+});
 
 
