3d30248d199fb8c13593a91c6139a5c873aa0fff
vagrant <vagrant@precise32.(none)>
2014-05-17 23:05:14 +0000

    changes to story schema

--- models/story.js
+++ models/story.js
@@ -4,32 +4,38 @@ var StorySchema = new mongoose.Schema({
 	username: String,
 	age: String,
 	location: String,
-	storyname: String, 
+	name: String, 
 	comments: String,
 	perpetrator: String,
 	victimGender: String,
 	perpetratorGender: String,
-	tag: String
+	tag: String,
+    src: String,
+    type: String,
+    image: String
 });
 
-StorySchema.statics.save = function(username, age, location, storyname, comments, perpetrator, victimGender, perpetratorGender, tag, done) {
+StorySchema.statics.save = function(username, age, location, storyname, comments, perpetrator, victimGender, perpetratorGender, tag, src, type, image, done) {
 	var Story = this;
 	Story.create({
 		username : username,
 		age : age,
 		location : location,
-		storyname : storyname,
+		name : storyname,
 		comments : comments,
 		perpetrator : perpetrator,
 		victimGender : victimGender,
 		perpetratorGender : perpetratorGender,
-		tag : tag
+		tag : tag,
+		src: src,
+		type: type,
+		image: image
 	}, function(err){
 		done(err);
 	});
 };
 
 
-var Story = mongoose.model('story', StorySchema);
+var Story = mongoose.model('Story', StorySchema);
 
 module.exports = Story;
