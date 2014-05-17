f8db5f0052bd6d3fa9eca0504e8c7c2a957e3284
Marie Feng <marie.feng@gmail.com>
2014-05-16 22:24:08 -0700

    test

--- models/user.js
+++ models/user.js
@@ -1,5 +1,17 @@
 var mongoose = require("mongoose");
-var hash = require('../util/hash');
+var Schema = mongoose.Schema;
+
+exports.User = new Schema({
+  	username: String,
+	email: String,
+	pronoun: String,
+	birthyear: String,
+	salt: String,
+	hash: String
+});
+
+
+/*var hash = require("../util/hash");
 
 var LocalUserSchema = new mongoose.Schema({
 	username: String,
@@ -8,16 +20,16 @@ var LocalUserSchema = new mongoose.Schema({
 	birthyear: String,
 	salt: String,
 	hash: String
-});
+});*/
 
-LocalUserSchema.statics.signup = function(username, password, email, pronoun, birthyear, done) {
+/*LocalUserSchema.statics.signup = function(username, password, email, pronoun, birthyear, done) {
 	var User = this;
 	User.find({ username: username }, function(err, user) {
 		if (err) throw err;
 		if (user.length > 0) {
 			done("Username already exists.", null);
 		} else {
-			hash(password, function(err, salt, hash){
+		hash(password, function(err, salt, hash){
 				if(err) throw err;
 				User.create({
 					username : username,
@@ -32,10 +44,8 @@ LocalUserSchema.statics.signup = function(username, password, email, pronoun, bi
 			});
 		}
 	})
-};
+};*/
 
+/*var User = mongoose.model('userauths', LocalUserSchema);
 
-
-var User = mongoose.model('userauths', LocalUserSchema);
-
-module.exports = User;
+module.exports = User;*/
