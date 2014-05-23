3bc22b1cde929ebe4f29091c44de761763197018
Marie Feng <marie.feng@gmail.com>
2014-05-22 23:53:56 -0700

    Base project

--- routes/routes.js
+++ routes/routes.js
@@ -3,15 +3,6 @@ var Week = require('../models/week.js');
 
 module.exports = function(app) {
 	app.get('/', function(req, res) {
-		return Week.find(function (err, weeks) {
-    		if (!err) {
-    			console.log("No error!");
-    			console.log(weeks);
-      			return res.render('index.ejs', { weeks: weeks});
-    		} else {
-    			console.log("Error!");
-      			return console.log(err);
-    		}
-  		});
+		res.render( 'index.ejs');
 	});
 };
